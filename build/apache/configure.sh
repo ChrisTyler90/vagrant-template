30898c1711fcef9e337995ce1bcd37f56746d2b6
Chris Tyler <me@chris-tyler.co.uk>
2015-12-16 10:21:49 +0000

    Moved logs from /vagrant/log to /vagrant/logs

--- build/apache/configure.sh
+++ build/apache/configure.sh
@@ -7,19 +7,19 @@ sudo service httpd stop > /dev/null 2>&1
 echo "- Configuring Apache"
 # Stop the Apache service
 sudo service httpd stop > /dev/null 2>&1
-# Run Apache as vagrant/log
+# Run Apache as vagrant
 sudo sed -i 's/User apache/User vagrant/g' /etc/http/conf/httpd.conf > /dev/null 2>&1
 sudo sed -i 's/Group apache/Group vagrant/g' /etc/http/conf/httpd.conf > /dev/null 2>&1
 # Remove the default Apache log directory
 rm /etc/httpd/logs > /dev/null 2>&1
 # Link the logs directory to our own
-ln -fs /vagrant/log /etc/httpd/logs > /dev/null 2>&1
+ln -fs /vagrant/logs /etc/httpd/logs > /dev/null 2>&1
 # Set up the VirtualHost
 cat << END > /etc/httpd/conf.d/000-default.conf
 <VirtualHost *:80>
     DocumentRoot "/vagrant/public_html"
     DirectoryIndex index.html index.php
-    ErrorLog "logs/error.log"
+    ErrorLog "logs/error.logs"
     CustomLog "logs/access.log" combined
 
     <Directory "/vagrant/public_html">
