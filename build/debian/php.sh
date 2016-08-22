45314a34420cf09c4437866963cd9ee7d1041d0c
Chris Tyler <hello@chris-tyler.co.uk>
2016-08-22 11:47:58 +0100

    Added php version to installation arguments

--- build/debian/php.sh
+++ build/debian/php.sh
@@ -16,11 +16,11 @@ echo " - Installing PHP Modules"
 for module in ${@:3}
 do
     echo "  - $module"
-    sudo apt-get install -y $module > /dev/null 2>&1
+    sudo apt-get install -y php$2-$module > /dev/null 2>&1
     sudo php5enmod $module > /dev/null 2>&1
 done;
 
 echo " - Configuring PHP"
 
 echo "  - Enabling PHP Errors"
-sudo sed -i 's/display_errors = Off/display_errors = On/' /etc/php5/apache2/php.ini
+sudo sed -i 's/display_errors = Off/display_errors = On/' /etc/php/$2/apache2/php.ini
